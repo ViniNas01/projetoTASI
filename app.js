a0f164188735aed5b9457219de615f4b40b0347e
unknown <ViniNas01>
2025-11-04 09:24:33 -0300

    feat: corrigindo conexão MySQL

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import './db.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3306;
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}.`);
--- db.js
+++ db.js
@@ -4,12 +4,17 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Vno!120601',
-    database: 'cliente_db',
-    port: 3000
+    database: 'cliente_db'
 });
 
 connection.connect(err => {
-    if (err) throw err;
+    if (err) {
+        console.error('Erro ao conectar ao MySQL: ', err.message);
+        return;
+    }
+
     console.log('Conectado ao MySQL!');
 });
+
+export default connection;
 
