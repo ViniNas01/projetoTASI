cfbe0827d835fb27ec4a91176bcd2b0025829882
unknown <ViniNas01>
2025-11-04 08:42:24 -0300

    feat: importando pacotes

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql2';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}.`);
+});
