e1bb329079bec5fd0d11d9ccd7cf41ab66a400a6
unknown <ViniNas01>
2025-11-04 15:21:18 -0300

    feat: criando GET e POST no controlador

---
+++ controller/clienteController.js
@@ -0,0 +1,40 @@
+import clienteModel from '../models/clienteModel';
+
+const clienteController = {
+    
+    showLoginForm(req, res) {
+        res.render('Login', {error: null, form: {} });
+    },
+
+    async handleLogin (req, res) {
+        try {
+            const { nome, endereco, telefone } = req.body;
+
+            if (!nome || !telefone) {
+                return res.render('login', { error: 'Nome e telefone são obrigatórios.', form: {nome, endereco, telefone } });
+            }
+
+            let cliente = await clienteModel.findByTelefone(telefone);
+            if(!cliente) {
+                cliente = await clienteModel.create({ nome, enderco,telefone});
+            }
+
+            req.session.cliente = { id: cliente.id, nome: cliente.nome, telefone: cliente.telefone};
+
+            return res.redirect('/home');
+
+        } catch (err) {
+            console.error(err);
+            return res.render('Login', { error: 'Erro no servidor. Tente Novamente.', form: req.body});
+        }
+    },
+
+    showHome(req, res) {
+        if(!req.session.cliente){
+            return res.redirect('/login');
+        }
+        res.render('home', {cliente: req.session.cliente });
+    }
+};
+
+module.exports = clienteController;
