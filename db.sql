44d32e5f9e0746ee2100fb7ff07d35249795c09c
unknown <ViniNas01>
2025-11-07 21:15:36 -0300

    feat: Adicionando tabela funcionario MySql

--- db.sql
+++ db.sql
@@ -7,4 +7,16 @@ CREATE TABLE clientes (
     nome VARCHAR(100) UNIQUE NOT NULL,
     endereco VARCHAR(100),
     telefone VARCHAR(100)
-)
+);
+
+CREATE TABLE contratos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  cliente_id INT NOT NULL,
+  profissional_id INT NOT NULL,
+  profissional_nome VARCHAR(100) NOT NULL,
+  categoria VARCHAR(100),
+  data_contratacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+  status VARCHAR(50) DEFAULT 'Ativo',
+  FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
